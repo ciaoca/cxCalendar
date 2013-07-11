f4eef9dd7f075086613e5bab98473a254786beff
ciaoca <ciaoca@gmail.com>
2013-07-11 10:46:14 +0800

    更新至 1.1

--- README.md
+++ README.md
@@ -1,7 +1,130 @@
+#jQuert.cxCalendar
+
 cxCalendar 是基于 jQuery 的日期选择器插件
 
-版本：
-+ jQuery v1.4.4+
-+ jQuery cxCalendar v1.0
+**版本：**
 
-http://code.ciaoca.com/jquery/cxcalendar/
+* jQuery v1.4.4+
+* jQuery cxCalendar v1.0
+
+文档：http://code.ciaoca.com/jquery/cxcalendar/
+
+示例：http://code.ciaoca.com/jquery/cxcalendar/demo/
+
+##【options 参数说明】
+
+<table>
+    <tr>
+        <th width="80">名称</th>
+        <th width="100">默认值</th>
+        <th>说明</th>
+    </tr>
+    <tr>
+        <td>begin_year</td>
+        <td>1950</td>
+        <td>起始年份</td>
+    </tr>
+    <tr>
+        <td>end_year</td>
+        <td>2030</td>
+        <td>结束年份</td>
+    </tr>
+    <tr>
+        <td>date</td>
+        <td>new Date()</td>
+        <td>
+            <p>默认日期。默认使用 javascript 获取当前日期，自定义需使用字符串。日期格式和 type 相同。</p>
+            <p>※ input 中的 value 值优先级要高级此值。</p>
+        </td>
+    </tr>
+    <tr>
+        <td>type</td>
+        <td>"yyyy-mm-dd"</td>
+        <td>日期格式。可设置为："yyyy-mm-dd" | "yyyy-m-d"</td>
+    </tr>
+    <tr>
+        <td>hyphen</td>
+        <td>"-"</td>
+        <td>日期连接符。可设置为："-" | "/" | "."</td>
+    </tr>
+    <tr>
+        <td>wday</td>
+        <td>0</td>
+        <td>周第一天。可设置为：0-6 之间的数字。
+            <p>0=星期日</p>
+            <p>1=星期一</p>
+            <p>2=星期二</p>
+            <p>3=星期三</p>
+            <p>4=星期四</p>
+            <p>5=星期五</p>
+            <p>6=星期六</p>
+        </td>
+    </tr>
+</table>
+
+##【language 语言配置说明】
+<table>
+    <tr>
+        <th width="80">名称</th>
+        <th width="400">默认值</th>
+        <th>说明</th>
+    </tr>
+    <tr>
+        <td>year</td>
+        <td>"年"</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>month</td>
+        <td>"月"</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td>month_list</td>
+        <td>["1","2","3","4","5","6","7","8","9","10","11","12"]</td>
+        <td>月份名称。</td>
+    </tr>
+    <tr>
+        <td>week_list</td>
+        <td>["日","一","二","三","四","五","六"]</td>
+        <td>星期名称。从星期日开始排序</td>
+    </tr>
+</table>
+
+##【使用方法】
+
+###载入 CSS 文件
+```html
+<link rel="stylesheet" href="css/jquery.cxcalendar.css">
+```
+
+###载入 JavaScript 文件
+```html
+<script src="js/jquery.js"></script>
+<script src="js/jquery.cxcalendar.js"></script>
+```
+
+###调用 cxCalendar
+```javascript
+// 直接调用
+$("#element_id").cxCalendar()
+
+// 自定义参数调用
+$("#element_id").cxCalendar({
+    begin_year:1950,
+    end_year:2030,
+    date:"1988/1/31",
+    type:"yyyy-mm-dd",
+    hyphen:"-",
+    wday:0
+});
+
+// 设置全局默认值，需在引入 <script src="js/jquery.cxselect.js"></script> 之后，调用之前设置
+$.cxCalendar.defaults.begin_year=1970;
+$.cxCalendar.language={
+    year:"",
+    month:"",
+    month_list:["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"],
+    week_list:["Sun","Mon","Tur","Wed","Thu","Fri","Sat"]
+};
+```
--- js/jquery.cxcalendar.js
+++ js/jquery.cxcalendar.js
@@ -1,13 +1,14 @@
 /*!
- * cxCalendar 1.0
- * date: 2012-12-07
+ * cxCalendar 1.1
+ * date: 2012-01-14
  * https://github.com/ciaoca/cxCalendar
  * (c) 2012 Ciaoca, http://ciaoca.com/
  */
 (function($){
-	$.fn.cxCalendar=function(settings){
+	$.fn.cxCalendar=function(settings,language){
 		if(this.length<1){return;};
 		settings=$.extend({},$.cxCalendar.defaults,settings);
+		language=$.extend({},$.cxCalendar.language,language);
 
 		var date_obj=this;
 
@@ -40,36 +41,35 @@
 		var date_name_month=new Array(31,28+leapYear(def_year),31,30,31,30,31,31,30,31,30,31);
 
 		// 定义每周的日期
-		var date_name_week=["日","一","二","三","四","五","六"];
+		var date_name_week=language.week_list;
 
 		// 定义周末
 		var saturday=6-settings.wday;
 		var sunday=(7-settings.wday>=7) ? 0 : (7-settings.wday);
 
 		// 创建选择器
-		var date_pane,date_title,date_table,block_bg,temp_html;
-		date_pane=$("<div></div>",{"class":"cxcalendar"}).appendTo("body");
-		date_title=$("<div></div>",{"class":"title"}).appendTo($(date_pane));
-		date_table=$("<table></table>").appendTo($(date_pane));
+		var date_pane,date_hd,date_set,date_table,block_bg,temp_html;
+		date_pane=$("<div></div>",{"class":"cxcalendar"});
+		date_hd=$("<div></div>",{"class":"date_hd"}).appendTo(date_pane);
+		date_table=$("<table></table>").appendTo(date_pane);
 
-		temp_html="<select class='year'>";
+		var date_txt,year_list,month_list;
+		date_hd.html("<a class='date_pre' href='javascript://' rel='pre'>&lt;</a><a class='date_next' href='javascript://' rel='next'>&gt;</a>");
+		date_txt=$("<div></div>",{"class":"date_txt"}).appendTo(date_hd);
+		date_set=$("<div></div>",{"class":"date_set"}).appendTo(date_hd);
+
+		temp_html="";
 		for(var i=settings.begin_year;i<=settings.end_year;i++){
 			temp_html+="<option value='"+i+"'>"+i+"</option>";
 		};
-		temp_html+="</select><span>年</span><select class='month'>";
-		for(var i=1;i<=12;i++){
-			temp_html+="<option value='"+i+"'>"+i+"</option>";
+		year_list=$("<select></select>",{"class":"year_set"}).html(temp_html).appendTo(date_set).val(def_year);
+		date_set.append(" - ");
+		
+		temp_html="";
+		for(var i=0;i<12;i++){
+			temp_html+="<option value='"+(i+1)+"'>"+language.month_list[i]+"</option>";
 		};
-		temp_html+="</select><span>月</span>";
-		date_title.html(temp_html);
-
-		var select_year,select_month,date_exit;
-		select_year=date_title.find("select.year");
-		select_month=date_title.find("select.month");
-		date_exit=date_title.find("em");
-
-		select_year.val(def_year);
-		select_month.val(def_month);
+		month_list=$("<select></select>",{"class":"month_set"}).html(temp_html).appendTo(date_set).val(def_month);
 
 		temp_html="<thead><tr>";
 		for(var i=0;i<7;i++){
@@ -87,17 +87,11 @@
 			temp_html+="</th>";
 		};
 		temp_html+="</tr></thead>";
-		temp_html+="<tfoot><td colspan='2'><a class='pre'>&lt;&lt;</a></td><td colspan='3'><a class='clear'>清除</a></td><td colspan='2'><a class='next'>&gt;&gt;</a></td></tfoot>";
 		temp_html+="<tbody></tbody>";
 		date_table.html(temp_html);
 
-		// 上一月、下一月、清除
-		var date_pre,date_next,date_clear;
-		date_pre=date_table.find("tfoot").find(".pre");
-		date_next=date_table.find("tfoot").find(".next");
-		date_clear=date_table.find("tfoot").find(".clear");
-		
-		// 背景遮挡层
+		// 面板及背景遮挡层插入到页面中
+		date_pane.appendTo("body");
 		block_bg=$("<div></div>").css({"display":"none","position":"absolute","top":"0","left":"0","background":"#fff","opacity":"0","z-index":"999"}).appendTo("body");
 
 		// 显示日期选择器
@@ -114,6 +108,7 @@
 			pane_top=((pane_top+pane_h+obj_h)>doc_h) ? pane_top-pane_h : pane_top+obj_h;
 			pane_left=((pane_left+pane_w)>doc_w) ? pane_left-(pane_w-obj_w) : pane_left;
 			
+			date_txt.html("<span class='y'>"+year_list.val()+"</span>"+language.year+"<span class='m'>"+language.month_list[month_list.val()-1]+"</span>"+language.month);
 			date_pane.css({"top":pane_top,"left":pane_left}).show();
 			block_bg.css({width:doc_w,height:doc_h}).show();
 		};
@@ -160,8 +155,8 @@
 			// 获取当月第一天
 			var firstday=(temp_date.getDay()-settings.wday<0) ? temp_date.getDay()-settings.wday+7 : temp_date.getDay()-settings.wday;
 			// 每月所需要的行数
-			//var tr_row=Math.ceil((date_name_month[m]+firstday)/7);
-			var tr_row=6;
+			var tr_row=Math.ceil((date_name_month[m]+firstday)/7);
+			//var tr_row=6;
 			var td_num,day_num,diff_now,diff_set;
 
 			for(var i=0;i<tr_row;i++){
@@ -201,22 +196,23 @@
 			};
 			date_table.find("tbody").html(temp_html);
 
-			select_year.val(y);
-			select_month.val(m+1);
+			date_txt.html("<span class='y'>"+y+"</span>"+language.year+"<span class='m'>"+language.month_list[m]+"</span>"+language.month);
+			year_list.val(y);
+			month_list.val(m+1);
 		};
 
 		// 选择日期函数
 		var dateSelect=function(d){
 			var temp_month,temp_day;
-				temp_month=select_month.val();
+				temp_month=month_list.val();
 				temp_day=d;
 			if(settings.type=="yyyy-mm-dd"){
-				temp_month="0"+select_month.val();
+				temp_month="0"+month_list.val();
 				temp_day="0"+d;
 				temp_month=temp_month.substr((temp_month.length-2),temp_month.length);
 				temp_day=temp_day.substr((temp_day.length-2),temp_day.length);
 			};
-			date_obj.val(select_year.val()+settings.hyphen+temp_month+settings.hyphen+temp_day);
+			date_obj.val(year_list.val()+settings.hyphen+temp_month+settings.hyphen+temp_day);
 			dateExit();
 		};
 
@@ -224,7 +220,29 @@
 		var dateExit=function(){
 			date_pane.hide();
 			block_bg.hide();
+			date_set.hide();
+			date_txt.show();
 		};
+
+		// 面板 <a> 事件
+		date_pane.delegate("a","click",function(){
+			if(!this.rel){return};
+
+			var _rel=this.rel;
+			switch(_rel){
+				case "pre":
+					dateChange(year_list.val(),parseInt(month_list.val(),10)-1);
+					break
+				case "next":
+					dateChange(year_list.val(),parseInt(month_list.val(),10)+1);
+					break
+				case "clear":
+					date_obj.val("");
+					dateChange(def_year,def_month);
+					dateExit();
+					break
+			};
+		});
 
 		// 选择日期事件
 		date_table.delegate("td","click",function(){
@@ -237,36 +255,23 @@
 		});
 
 		// 显示面板事件
-		date_obj.bind("click",function(){
-			dateShow();
+		date_obj.bind("click",dateShow);
+
+		// 关闭面板事件
+		block_bg.bind("click",dateExit);
+
+		// 显示年月选择
+		date_txt.bind("click",function(){
+			date_txt.hide();
+			date_set.show();
 		});
 
 		// 更改年月事件
-		select_year.bind("change",function(){
-			dateChange(select_year.val(),select_month.val());
+		year_list.bind("change",function(){
+			dateChange(year_list.val(),month_list.val());
 		});
-		select_month.bind("change",function(){
-			dateChange(select_year.val(),select_month.val());
-		});
-
-		// 上月、下月事件
-		date_pre.bind("click",function(){
-			dateChange(select_year.val(),parseInt(select_month.val(),10)-1);
-		});
-		date_next.bind("click",function(){
-			dateChange(select_year.val(),parseInt(select_month.val(),10)+1);
-		});
-
-		// 清除事件
-		date_clear.bind("click",function(){
-			date_obj.val("");
-			dateChange(def_year,def_month);
-			dateExit();
-		});
-
-		// 关闭面板事件
-		block_bg.bind("click",function(){
-			dateExit();
+		month_list.bind("change",function(){
+			dateChange(year_list.val(),month_list.val());
 		});
 
 		// 第一次初始化
@@ -281,5 +286,25 @@
 		type:"yyyy-mm-dd",	// 日期格式
 		hyphen:"-",			// 日期链接符
 		wday:0				// 周第一天
+	},language:{
+		/* 中文 */
+		year:"年",
+		month:"月",
+		month_list:["1","2","3","4","5","6","7","8","9","10","11","12"],
+		week_list:["日","一","二","三","四","五","六"]
+		
+		/* English 
+		year:"",
+		month:"",
+		month_list:["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"],
+		week_list:["Sun","Mon","Tur","Wed","Thu","Fri","Sat"]
+		*/
+		
+		/* 日本語 
+		year:"年",
+		month:"月",
+		month_list:["1","2","3","4","5","6","7","8","9","10","11","12"],
+		week_list:["日","月","火","水","木","金","土"]
+		*/
 	}};
 })(jQuery);
